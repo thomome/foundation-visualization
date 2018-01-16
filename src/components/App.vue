3f1fab52250a8bd5f5c32d721d9c5161ce5dad77
Thomas Guthruf <thomasguthruf@gmail.com>
2018-01-16 17:35:28 +0100

    added more space for levels/phases selection box

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,8 +6,8 @@
         <div class="fields">
           <div class="fifteen wide field">
             <div class="three fields">
-              <foundation-select size="five" :data="this.$store.getters.scopes2" label="scope2" icon="tags"></foundation-select>
-              <foundation-select size="five" :data="this.$store.getters.scopes1" label="scope1" icon="chain"></foundation-select>
+              <foundation-select size="six" :data="this.$store.getters.scopes2" label="scope2" icon="tags"></foundation-select>
+              <foundation-select size="eight" :data="this.$store.getters.scopes1" label="scope1" icon="chain"></foundation-select>
               <foundation-filter v-show="chainType == 1" selectType="chain" :data="this.$store.getters.innochains" label="innochain" icon="rocket"></foundation-filter>
               <foundation-filter v-show="chainType == 2" selectType="chain" :data="this.$store.getters.educhains" label="educhain" icon="graduation cap"></foundation-filter>
             </div>
